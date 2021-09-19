01e19566f46760ee802946e7ac9ce3c28b7939c7
onimo <onimo07@gmail.com>
2021-09-19 10:35:50 +0300

    Добавил второй вариант

--- Infrastructure/xStorage.cs
+++ Infrastructure/xStorage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WSAMP.Infrastructure
+namespace TestScrollSet.Infrastructure
 {
     class xStorage
     {
