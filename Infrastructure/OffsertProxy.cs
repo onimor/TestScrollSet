56c0383cb0c08f25bb277b886d89fef0e765cbfc
onimo <onimo07@gmail.com>
2021-09-20 12:36:21 +0300

    Обновил реализацию от Элд Хасп

--- Infrastructure/OffsertProxy.cs
+++ Infrastructure/OffsertProxy.cs
@@ -89,19 +89,63 @@ namespace AttachedProperties
                 _ = BindingOperations.SetBinding(this, _VerticalOffsetProperty, verticalBinding);
                 _ = BindingOperations.SetBinding(this, _HorizontalOffsetProperty, horizontalBinding);
 
-
-                scrollViewer.ScrollToBottom();
-                scrollViewer.ScrollToRightEnd();
-
-                scrollViewer.Dispatcher.BeginInvoke(SetOffsetAction, scrollViewer, vertOffset, horizOffset);
+                OffsetInitAsync(scrollViewer, horizOffset, vertOffset);
             }
-        }
 
-        private static readonly Action<System.Windows.Controls.ScrollViewer, double, double> SetOffsetAction = (sv, v, h) =>
-        {
-            sv.ScrollToVerticalOffset(v);
-            sv.ScrollToHorizontalOffset(h);
-        };
+            private static async void OffsetInitAsync(System.Windows.Controls.ScrollViewer scrollViewer, double horizontal, double vertical)
+            {
+                HV hv = new HV();
+                double horizOld;
+                double vertOld;
+
+                await scrollViewer.Dispatcher.BeginInvoke(GetSizeAction, scrollViewer, hv);
+                do
+                {
+                    double horiz = horizontal;
+                    if (horiz > hv.width)
+                        horiz = hv.width;
+                    double vert = vertical;
+                    if (vert > hv.height)
+                        vert = hv.height;
+                    await scrollViewer.Dispatcher.BeginInvoke(SetOffsetAction, scrollViewer, horiz, vertical);
+                    (horizOld, vertOld) = (hv.horiz, hv.vert);
+                    await scrollViewer.Dispatcher.BeginInvoke(GetOffsetAction, scrollViewer, hv);
+                } while (!(hv.IsEnd || hv.OffsetEquals(horizontal, vertical) || (horizOld, vertOld) == (hv.horiz, hv.vert)));
+            }
+
+            private class HV
+            {
+                public double width;
+                public double height;
+                public double horiz;
+                public double vert;
+
+                public bool IsEnd => width == horiz && height == vert;
+                public bool OffsetEquals(double horizontal, double vertical)
+                    => horizontal == horiz && vertical == vert;
+                public override string ToString() => $"({width} - {height}); ({horiz} - {vert})";
+            }
+
+            private static readonly Action<System.Windows.Controls.ScrollViewer, HV> GetSizeAction = (sv, hv) =>
+            {
+                hv.width = sv.ActualWidth;
+                hv.height = sv.ActualHeight;
+            };
+
+            private static readonly Action<System.Windows.Controls.ScrollViewer, HV> GetOffsetAction = (sv, hv) =>
+            {
+                hv.horiz = sv.HorizontalOffset;
+                hv.vert = sv.VerticalOffset;
+                GetSizeAction(sv, hv);
+            };
+
+
+            private static readonly Action<System.Windows.Controls.ScrollViewer, double, double> SetOffsetAction = (sv, h, v) =>
+            {
+                sv.ScrollToHorizontalOffset(h);
+                sv.ScrollToVerticalOffset(v);
+            };
+        }
 
     }
 }
